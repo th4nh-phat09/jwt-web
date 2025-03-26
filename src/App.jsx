97615785d161dd0e88d6050fafa0b3e2dce97bad
th4nh-phat09 <thanphat354@gmail.com>
2025-03-26 13:40:06 +0700

    logout_account

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom'
 import Login from '~/pages/Login'
 import Dashboard from '~/pages/Dashboard'
+
+const ProtectedRoutes = () => {
+  const user = JSON.parse(localStorage.getItem('userInfo'))
+  // console.log(user)
+  if (!user) return <Navigate to="/login" replace={true} />
+  return <Outlet />
+}
+
+const UnauthorizedRoutes = () => {
+  const user = JSON.parse(localStorage.getItem('userInfo'))
+  if (user) return <Navigate to="/dashboard" replace={true} />
+  return <Outlet />
+}
+
 
 function App() {
   return (
@@ -8,9 +22,17 @@ function App() {
       <Route path='/' element={
         <Navigate to="/login" replace={true} />
       } />
-
-      <Route path='/login' element={<Login />} />
-      <Route path='/dashboard' element={<Dashboard />} />
+      <Route></Route>
+      <Route element={<UnauthorizedRoutes />}>
+        {/* <Outlet /> của react-router-dom sẽ chạy vào các child route trong này */}
+        <Route path='/login' element={<Login />} />
+        {/* Sau này sẽ còn nhiều Route nữa ở đây...vv */}
+      </Route>
+      <Route element={<ProtectedRoutes />}>
+        {/* <Outlet /> của react-router-dom sẽ chạy vào các child route trong này */}
+        <Route path='/dashboard' element={<Dashboard />} />
+        {/* Sau này sẽ còn nhiều Route nữa ở đây...vv */}
+      </Route>
     </Routes>
   )
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,10 +6,13 @@ import CircularProgress from '@mui/material/CircularProgress'
 import Divider from '@mui/material/Divider'
 import authorizedAxiosInstance from '~/utils/authorizedAxios'
 import { API_ROOT } from '~/utils/constants'
+import Button from '@mui/material/Button'
+import { useNavigate } from 'react-router-dom'
+
 
 function Dashboard() {
   const [user, setUser] = useState(null)
-
+  const navigate = useNavigate()
   useEffect(() => {
     const fetchData = async () => {
       const res = await authorizedAxiosInstance.get(`${API_ROOT}/v1/dashboards/access`)
@@ -17,6 +20,16 @@ function Dashboard() {
     }
     fetchData()
   }, [])
+  const handleLogout = async () => {
+    // Cách 1 xóa token ở LocalStorage nếu lưu token ở localStorage
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('refreshToken')
+    localStorage.removeItem('userInfo')
+
+    //cách 2 thì gọi tới api xóa token ở server khi lưu ở HttpOnly Cookie
+    await authorizedAxiosInstance.delete(`${API_ROOT}/v1/users/logout`)
+    navigate('/login')
+  }
 
   if (!user) {
     return (
@@ -50,6 +63,16 @@ function Dashboard() {
       </Alert>
 
       <Divider sx={{ my: 2 }} />
+      <Button
+        type="button"
+        variant="contained"
+        color="info"
+        size="large"
+        sx={{ mt: 2, maxWidth: 'min-content', alignSelf: 'flex-end' }}
+        onClick={handleLogout}
+      >
+        Logout
+      </Button>
     </Box>
   )
 }
